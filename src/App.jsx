c9d29526e36fc087afc89beec8bd824f5adc9556
Mokshith213 <mokshimokshith213@gmail.com>
2023-03-01 23:00:03 +0530

    https://github.com/Mokshith213/React.git

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import Refs from "./Refs"
 
-const App = () => {
-  return (
-    <div>
-      <Refs/>
-    </div>
-  )
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  componentDidMount()
+  {
+    let btn=document.querySelector('form')
+
+    btn.addEventListener("click",(e)=>
+    {
+        e.preventDefault()
+        let username=document.getElementById("name").value
+        let password=document.getElementById("password").value
+        console.log({username , password});
+    })
+  }
+  
+    render() {
+    return (
+      <div>
+        <form action="">
+
+            <label htmlFor="">name:</label>
+            <br />
+            <input type="text" id="name" />
+            <br />
+            <label htmlFor="">password:</label>
+            <br />
+            <input type="text" id='password' />
+            <br />
+            <button>Submit</button>
+        </form>
+   
+      </div>
+    )
+  }
 }
-
-export default App
